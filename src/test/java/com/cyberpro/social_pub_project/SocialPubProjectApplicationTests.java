87e39cea202cf59e74ce50ec9e8b099379199f1c
Tal-710 <talbrachya10@gmail.com>
2025-01-08 12:10:46 +0300

    init spring-boot project

---
+++ src/test/java/com/cyberpro/social_pub_project/SocialPubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberpro.social_pub_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialPubProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
