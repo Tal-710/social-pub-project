42bba246206ad5f9badb6178192b9ed507148101
Tal-710 <talbrachya10@gmail.com>
2025-01-08 12:10:46 +0300

    init spring-boot project

---
+++ src/main/java/com/cyberpro/social_pub_project/SocialPubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cyberpro.social_pub_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialPubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialPubProjectApplication.class, args);
+	}
+
+}
