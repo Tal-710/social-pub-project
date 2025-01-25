b2d32c9d2b2f1fab4e16c18e4b12788d394eff9f
Tal-710 <talbrachya10@gmail.com>
2025-01-25 15:35:40 +0200

    fix/css

--- src/main/java/com/cyberpro/social_pub_project/controller/LoginController.java
+++ src/main/java/com/cyberpro/social_pub_project/controller/LoginController.java
@@ -1,15 +1,12 @@
 package com.cyberpro.social_pub_project.controller;
 
-import com.cyberpro.social_pub_project.dto.ReCaptchaResponse;
+
 import com.cyberpro.social_pub_project.service.ReCaptchaService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 @Controller
